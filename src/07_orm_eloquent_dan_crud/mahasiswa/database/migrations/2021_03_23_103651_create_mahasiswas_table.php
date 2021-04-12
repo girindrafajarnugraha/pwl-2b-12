060482bd4ad5ab7d653f04dc2b66bb371118ee64
girindrafajarnugraha <indra.paradinata@gmail.com>
2021-04-12 19:35:53 +0700

    Laporan Praktikum 7

---
+++ src/07_orm_eloquent_dan_crud/mahasiswa/database/migrations/2021_03_23_103651_create_mahasiswas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMahasiswasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mahasiswa', function (Blueprint $table) {
+            $table->string('nim',20)->primary();
+            $table->string('nama',30);
+            $table->string('kelas',10);
+            $table->string('jurusan',30);
+            $table->string('no_handphone',16);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mahasiswa');
+    }
+}
